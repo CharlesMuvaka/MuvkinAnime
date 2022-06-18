d22cad11516e638b5aea002806d2134894898d1f
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 00:30:18 +0300

    create a user password constant to store the users password in his device

--- app/src/main/java/com/example/muvkinanim/models/Constants.java
+++ app/src/main/java/com/example/muvkinanim/models/Constants.java
@@ -3,5 +3,6 @@ package com.example.muvkinanim.models;
 public class Constants {
 
     public static final String USERNAME =  "username";
+    public static final String USER_PASSWORD =  "password";
 
 }
