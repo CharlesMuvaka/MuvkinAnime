f8027f89f9d90f9ee21a595c5e54c952e5046d89
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 00:06:06 +0300

    add a viewbinding instance to inflate the sign layout resource an find its views

--- app/src/main/java/com/example/muvkinanim/SignUpActivity.java
+++ app/src/main/java/com/example/muvkinanim/SignUpActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.muvkinanim.databinding.ActivitySignUpBinding;
+
 public class SignUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding signBind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        signBind = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(signBind.getRoot());
     }
 }
