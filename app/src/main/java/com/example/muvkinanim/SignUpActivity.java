4a829e9c03ee6323ea0aef91a7f77b4e1d72e90f
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 00:08:10 +0300

    initialize a firebase auth to create users in firebase database using their emails and password

--- app/src/main/java/com/example/muvkinanim/SignUpActivity.java
+++ app/src/main/java/com/example/muvkinanim/SignUpActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.muvkinanim.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
     ActivitySignUpBinding signBind;
+    FirebaseAuth myAuth;
+    FirebaseAuth.AuthStateListener myAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         signBind = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(signBind.getRoot());
+
+        myAuth = FirebaseAuth.getInstance();
+
     }
 }
