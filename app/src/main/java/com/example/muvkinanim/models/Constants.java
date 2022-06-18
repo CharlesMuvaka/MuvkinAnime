f577e07fd08085f54e2aac53128f6663d457d70e
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 00:51:27 +0300

    create a user constant to act as users reference in the firebase realtime database

--- app/src/main/java/com/example/muvkinanim/models/Constants.java
+++ app/src/main/java/com/example/muvkinanim/models/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
     public static final String USERNAME =  "username";
     public static final String USER_PASSWORD =  "password";
+    public static final String USERS =  "users";
 
 }
