3fabc725f34b90ce29ea150a57c968e007334693
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 02:49:28 +0300

    override the onOptionsCreateMenu and onItemOptionSelected method to inflate the overflow menu layout and handle clicks on the items selected

--- app/src/main/java/com/example/muvkinanim/MainActivity.java
+++ app/src/main/java/com/example/muvkinanim/MainActivity.java
@@ -28,7 +28,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
 
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.logout){
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 
     public void navigationBarClick(BottomNavigationView bottomNav){
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
