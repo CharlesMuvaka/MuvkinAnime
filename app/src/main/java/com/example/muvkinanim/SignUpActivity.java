ab1cf86ebe152c36b626642e8aae4c540c8f1ce0
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 01:54:26 +0300

    fix: proper collection of the user display name from firebase

--- app/src/main/java/com/example/muvkinanim/SignUpActivity.java
+++ app/src/main/java/com/example/muvkinanim/SignUpActivity.java
@@ -1,14 +1,24 @@
 package com.example.muvkinanim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.muvkinanim.databinding.ActivitySignUpBinding;
+import com.example.muvkinanim.models.Constants;
+import com.example.muvkinanim.models.User;
+import com.example.muvkinanim.models.Validator;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     ActivitySignUpBinding signBind;
@@ -27,10 +37,86 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         myData = PreferenceManager.getDefaultSharedPreferences(this);
         myDataEditor = myData.edit();
 
+        signBind.btnSubmit.setOnClickListener(this);
+        createAuthListener();
+
+    }
+
+    private void createAuthListener() {
+        myAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+                if (currentUser != null){
+                    Intent mainIntent = new Intent(SignUpActivity.this, MainActivity.class);
+                    mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();
+
+                }
+            }
+        };
     }
 
     @Override
     public void onClick(View v) {
 
+        if (v == signBind.btnSubmit){
+
+            createUser();
+        }
+
+    }
+
+    private void createUser() {
+        String userName = signBind.userName.getEditText().getText().toString().trim();
+        String userPhone = signBind.userPhone.getEditText().getText().toString().trim();
+        String userEmail = signBind.userTicket.getEditText().getText().toString().trim();
+        String password = signBind.userSeat.getEditText().getText().toString().trim();
+        String pass = signBind.userCoach.getEditText().getText().toString().trim();
+
+
+
+
+        if (!Validator.validateName(signBind.userName) || !Validator.validatePhone(signBind.userPhone) || !Validator.valiadateEmail(signBind.userTicket) || !Validator.validatePassword(signBind.userSeat) || !Validator.confirmPassword(signBind.userCoach, signBind.userSeat) ){
+            return;
+        }
+
+        myDataEditor.putString(Constants.USERNAME, userName).apply();
+        myDataEditor.putString(Constants.USER_PASSWORD, password).apply();
+
+        myAuth.createUserWithEmailAndPassword(userEmail, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                FirebaseUser user = task.getResult().getUser();
+                String userId = user.getUid();
+
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(Constants.USERS);
+                User newUser = new User(userId, userName,userPhone,userEmail);
+                ref.child(userId).setValue(newUser);
+
+
+                UserProfileChangeRequest request = new UserProfileChangeRequest.Builder().setDisplayName(userName).build();
+                user.updateProfile(request);
+
+                Toast.makeText(this, "Successfully created " + task.getResult().getUser().getDisplayName(), Toast.LENGTH_LONG
+                ).show();
+
+            }else{
+                Toast.makeText(this, "User Creation Failed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myAuth.addAuthStateListener(myAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        myAuth.removeAuthStateListener(myAuthListener);
     }
 }
