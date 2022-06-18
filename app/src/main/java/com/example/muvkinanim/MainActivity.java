dfeef7a891805621d15b8ba5617b2fa0fee9e55b
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 02:47:50 +0300

    add a method on the bottom navigation to handle clicks on the bottom views

--- app/src/main/java/com/example/muvkinanim/MainActivity.java
+++ app/src/main/java/com/example/muvkinanim/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.muvkinanim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.muvkinanim.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding mainBind;
@@ -15,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
         mainBind = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBind.getRoot());
 
+        mainBind.bottomNav.setSelectedItemId(R.id.home);
+        navigationBarClick(mainBind.bottomNav);
+
+
+    }
+
+
+
+    public void navigationBarClick(BottomNavigationView bottomNav){
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        return true;
+                    case R.id.favourites:
+                        startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
 
     }
 }
