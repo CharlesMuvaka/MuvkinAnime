2986a9c8d8655e72efd487396356f0ab206e1fa0
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 16:24:11 +0300

    create a login method an listening method to authenticate users with their email and password and the listener to check whether the user exists

--- app/src/main/java/com/example/muvkinanim/LoginActivity.java
+++ app/src/main/java/com/example/muvkinanim/LoginActivity.java
@@ -1,15 +1,27 @@
 package com.example.muvkinanim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.muvkinanim.databinding.ActivityLoginBinding;
+import com.example.muvkinanim.models.Constants;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     ActivityLoginBinding logBind;
+    FirebaseAuth myAuth;
+    FirebaseAuth.AuthStateListener myAuthListener;
+    SharedPreferences userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(logBind.getRoot());
 
         logBind.btnSign.setOnClickListener(this);
+        myAuth = FirebaseAuth.getInstance();
+        userData = PreferenceManager.getDefaultSharedPreferences(this);
+        createAuthListener();
+    }
+
+    private void createAuthListener() {
+         myAuthListener = new FirebaseAuth.AuthStateListener() {
+             @Override
+             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                 FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                 if (currentUser != null){
+                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                 }else{
+                     Toast.makeText(LoginActivity.this, "User doesn't exist", Toast.LENGTH_SHORT).show();
+                 }
+
+             }
+         };
     }
 
     @Override
@@ -25,7 +55,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if (v == logBind.btnSign){
             Intent newIntent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(newIntent);
+        }else if (v == logBind.btnLogin){
+            login();
+        }else if(v == logBind.checkBox){
+            logBind.userName.getEditText().setText(userData.getString(Constants.USERNAME, null));
+            logBind.userPhone.getEditText().setText(userData.getString(Constants.USER_PASSWORD, null));
         }
 
+    }
+
+    private void login() {
+        String email = logBind.userName.getEditText().getText().toString().trim();
+        String pass = logBind.userPhone.getEditText().getText().toString().trim();
+
+        myAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                Toast.makeText(this, "Welcome "+ Objects.requireNonNull(task.getResult().getUser()).getDisplayName(), Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
