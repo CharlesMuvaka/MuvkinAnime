b2e04dc27337a18e80ad19c42e2cfcabcd4e20f1
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 14:57:29 +0300

    create an api call to retreve all meals based on the first letter added as a query parameter of the base url

--- app/src/main/java/com/example/muvkinanim/retrofit/ApiMeal.java
+++ app/src/main/java/com/example/muvkinanim/retrofit/ApiMeal.java
@@ -1,9 +1,15 @@
 package com.example.muvkinanim.retrofit;
 
+import com.example.muvkinanim.models.Food;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiMeal {
 
     @GET
-    Call<>
+    Call<Food> getMeals(
+            @Query("f") String letter
+    );
 }
