bff7e1c0a3b028682b2f8224bc15acd82a3cef31
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 16:01:30 +0300

    add a method to make an api call for meals beginning with letter G

--- app/src/main/java/com/example/muvkinanim/MainActivity.java
+++ app/src/main/java/com/example/muvkinanim/MainActivity.java
@@ -2,19 +2,34 @@ package com.example.muvkinanim;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.example.muvkinanim.adapters.RecyclerViewAdapter;
 import com.example.muvkinanim.databinding.ActivityMainBinding;
+import com.example.muvkinanim.models.Food;
+import com.example.muvkinanim.models.Meal;
+import com.example.muvkinanim.retrofit.ApiMeal;
+import com.example.muvkinanim.retrofit.RetrofitClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding mainBind;
+    List<Meal> apiMeals;
+    RecyclerViewAdapter adp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +39,58 @@ public class MainActivity extends AppCompatActivity {
 
         mainBind.bottomNav.setSelectedItemId(R.id.home);
         navigationBarClick(mainBind.bottomNav);
+        makeApiCall();
 
+
+
+    }
+
+    private void makeApiCall() {
+        ApiMeal getMeals = RetrofitClient.mealClient();
+        Call<Food> foodCall = getMeals.getMeals("g");
+
+        foodCall.enqueue(new Callback<Food>() {
+            @Override
+            public void onResponse(Call<Food> call, Response<Food> response) {
+                if (response.isSuccessful()){
+
+                    apiMeals = response.body().getMeals();
+                    adp = new RecyclerViewAdapter(apiMeals, MainActivity.this);
+                    mainBind.recView.setAdapter(adp);
+                    mainBind.recView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                    mainBind.recView.setHasFixedSize(true);
+                    successful();
+                }else{
+                    unSuccessFul();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Food> call, Throwable t) {
+                failed();
+            }
+        });
+    }
+
+    public void successful(){
+        mainBind.progress.setVisibility(View.GONE);
+        mainBind.welcome.setVisibility(View.VISIBLE);
+        mainBind.recView.setVisibility(View.VISIBLE);
+    }
+
+    public void unSuccessFul(){
+        mainBind.progress.setVisibility(View.GONE);
+        mainBind.welcome.setVisibility(View.VISIBLE);
+        mainBind.welcome.setText("Check your internet connection and PLEASE TRY AGAIN ");
+        mainBind.recView.setVisibility(View.GONE);
+
+    }
+
+    public void failed(){
+        mainBind.progress.setVisibility(View.GONE);
+        mainBind.welcome.setVisibility(View.VISIBLE);
+        mainBind.welcome.setText("Something happened Please try again later ");
+        mainBind.recView.setVisibility(View.GONE);
 
     }
 
