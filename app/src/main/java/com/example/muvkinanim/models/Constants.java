0ea0a2a2e2afe1352fabea4fbcd8175d8f7feefd
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 14:47:18 +0300

    create a base url constant to store Meal apis base url

--- app/src/main/java/com/example/muvkinanim/models/Constants.java
+++ app/src/main/java/com/example/muvkinanim/models/Constants.java
@@ -1,7 +1,7 @@
 package com.example.muvkinanim.models;
 
 public class Constants {
-
+    public static final String URL = "https://www.themealdb.com/api/json/v1/1/";
     public static final String USERNAME =  "username";
     public static final String USER_PASSWORD =  "password";
     public static final String USERS =  "users";
