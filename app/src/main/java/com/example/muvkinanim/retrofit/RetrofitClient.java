61b7058c6ae9ed04c236cbb62136f63c344da254
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 14:45:52 +0300

    initial a retrofit client singleton that makes http request using the mealApi base url

--- app/src/main/java/com/example/muvkinanim/retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/muvkinanim/retrofit/RetrofitClient.java
@@ -1,4 +1,17 @@
 package com.example.muvkinanim.retrofit;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+
+    public static ApiMeal mealClient(){
+        if(retrofit == null){
+
+            retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit.create(ApiMeal.class);
+    }
 }
