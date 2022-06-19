f9027c55957d5ae5673c7150d68128e27067bf99
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-19 15:25:29 +0300

    add picasso dependencies

---
+++ app/src/main/java/com/example/muvkinanim/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,16 @@
+package com.example.muvkinanim;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter {
+
+    public static class MealViewHolder extends RecyclerView.ViewHolder {
+
+        public MealViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
