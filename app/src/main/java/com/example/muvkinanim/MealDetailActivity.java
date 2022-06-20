0eb1e50dbf94d7fcf318da298d10f22efb02d9a3
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-20 04:36:08 +0300

    set up navigation clicks to easen up the users journey

--- app/src/main/java/com/example/muvkinanim/MealDetailActivity.java
+++ app/src/main/java/com/example/muvkinanim/MealDetailActivity.java
@@ -1,14 +1,58 @@
 package com.example.muvkinanim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.muvkinanim.adapters.FragmentMealAdapter;
+import com.example.muvkinanim.databinding.ActivityMealDetailBinding;
+import com.example.muvkinanim.models.Meal;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
 
 public class MealDetailActivity extends AppCompatActivity {
+    ActivityMealDetailBinding mealDetBind;
+    FragmentMealAdapter adp;
+    List<Meal> mealList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meal_detail);
+        mealDetBind = ActivityMealDetailBinding.inflate(getLayoutInflater());
+        setContentView(mealDetBind.getRoot());
+
+        Intent newIntent = getIntent();
+        int position = newIntent.getIntExtra("position", 0);
+        mealList  = (List<Meal>) newIntent.getSerializableExtra("FragmentMeals");
+        adp = new FragmentMealAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mealList);
+        mealDetBind.viewPager.setAdapter(adp);
+        mealDetBind.viewPager.setCurrentItem(position);
+
+        setupNavigation(mealDetBind.bottomView);
+    }
+
+    private void setupNavigation(BottomNavigationView bottomView) {
+
+        bottomView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(MealDetailActivity.this, MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.favourites:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
