669afb72f0a14af46a87b5843088ddab884b546c
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-20 03:58:40 +0300

    bind fragment views with the meal fragment data

--- app/src/main/java/com/example/muvkinanim/fragments/MealDetailFragment.java
+++ app/src/main/java/com/example/muvkinanim/fragments/MealDetailFragment.java
@@ -9,13 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.muvkinanim.R;
+import com.example.muvkinanim.databinding.FragmentMealDetailBinding;
 import com.example.muvkinanim.models.Meal;
+import com.squareup.picasso.Picasso;
 
 import java.io.Serializable;
 
 
 public class MealDetailFragment extends Fragment {
     Meal fragmentMeal;
+    FragmentMealDetailBinding detBind;
 
 
 
@@ -45,7 +48,26 @@ public class MealDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_meal_detail, container, false);
+        detBind = FragmentMealDetailBinding.inflate(inflater, container, false);
+        View v = detBind.getRoot();
+
+        Picasso.get().load(fragmentMeal.getStrMealThumb()).into(detBind.image);
+        detBind.name.setText(fragmentMeal.getStrMeal());
+        detBind.ingNum.setText(fragmentMeal.getIdMeal());
+        detBind.ingre.setText("Meal Ingredients");
+        detBind.ingre1.setText(fragmentMeal.getStrIngredient1());
+        detBind.ingre2.setText(fragmentMeal.getStrIngredient2());
+        detBind.ingre3.setText(fragmentMeal.getStrIngredient3());
+        detBind.ingre4.setText(fragmentMeal.getStrIngredient4());
+        detBind.ingre5.setText(fragmentMeal.getStrIngredient5());
+        detBind.instr.setText("Meal Recipe Measures");
+        detBind.instr1.setText(fragmentMeal.getStrMeasure1());
+        detBind.instr2.setText(fragmentMeal.getStrMeasure2());
+        detBind.instr3.setText(fragmentMeal.getStrMeasure3());
+        detBind.instr4.setText(fragmentMeal.getStrMeasure4());
+        detBind.instr5.setText(fragmentMeal.getStrMeasure5());
+        detBind.desc.setText(fragmentMeal.getStrInstructions());
+
         return v;
     }
 }
