0d13e65085009e2adaea295509bb9f95a0d0726e
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-06-23 06:19:39 +0300

    add tests to confirm if the editexts can be filled with data

---
+++ app/src/androidTest/java/com/example/muvkinanim/LoginActivityInstrumentedTest.java
@@ -0,0 +1,48 @@
+package com.example.muvkinanim;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.endsWith;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mainRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void typeEditTextInputs(){
+
+        onView(allOf(
+                isDescendantOfA(withId(R.id.userName)),
+                withClassName(endsWith("TextInputEditText"))
+        )).perform(typeText("muvakacharles@gmail.com"));
+
+        onView(allOf(isDescendantOfA(withId(R.id.userPhone)),withClassName(endsWith("TextInputEditText")))).perform(typeText("0768761611"));
+
+    }
+
+    @Test
+    public void clickLoginButton(){
+        onView(withId(R.id.btnLogin)).perform(click());
+    }
+
+    @Test
+    public void clickSinUpButton(){
+
+        onView(withId(R.id.btnSign)).perform(click());
+    }
+}
+
+
